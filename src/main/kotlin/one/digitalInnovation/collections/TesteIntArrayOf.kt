28c31db10c8f36a1ad1cebcb0fa17eee88f4cb2e
julioho7 <julioholiveira7@gmail.com>
2022-07-01 20:28:55 -0300

    Aprendendo intArrayOf

--- src/main/kotlin/one/digitalInnovation/collections/TesteIntArrayOf.kt
+++ src/main/kotlin/one/digitalInnovation/collections/TesteIntArrayOf.kt
@@ -1,5 +1,18 @@
 package one.digitalInnovation.collections
 
 fun main() {
+    val values = intArrayOf(2, 3, 4, 1, 10, 7) //Nesse caso, não precisa declarar o tamanho logo de início. Ele é mais flexível.
+
+    values.forEach {
+        println(it)
+
+    }
+
+    println("---------------------------------------------------")
+    values.sort() //Irá organizar o Array, por padrão, em ordem crescente.
+    values.forEach {
+        println(it)
+
+    }
 
 }
