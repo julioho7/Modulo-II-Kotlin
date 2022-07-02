574ce5028aa442216760803d8b4f1d8408a9497f
julioho7 <julioholiveira7@gmail.com>
2022-07-02 18:31:46 -0300

    Aprendendo a trabalhar com groupBy

--- src/main/kotlin/one/digitalInnovation/collections/TesteList.kt
+++ src/main/kotlin/one/digitalInnovation/collections/TesteList.kt
@@ -1,26 +1,39 @@
 package one.digitalInnovation.collections
 
 fun main() {
-    val funcionario1 = Funcionario("João João", 1000.0)
-    val funcionario2 = Funcionario("Pedro Pedro", 2000.0)
-    val funcionario3 = Funcionario("Maria Maria", 4000.0)
+    val funcionario1 = Funcionario("João", 2000.0, "CLT")
+    val funcionario2 = Funcionario("Pedro", 1500.0, "PJ")
+    val funcionario3 = Funcionario("Maria", 4000.0, "CLT")
 
     val funcionarios = listOf(funcionario1, funcionario2, funcionario3)
 
+    println("Printando todos os da lista na ordem que estão:")
     funcionarios.forEach { println(it) }
 
     println("-----------------------------------------------")
-    println(funcionarios.find { it.nome == "Maria Maria" })
+    println("Printando o nome setado: ")
+    println(funcionarios.find { it.nome == "Maria" })
+
+    println("-----------------------------------------------")
+    println("Print de forma a ordenar por salário: ")
+    funcionarios
+        .sortedBy { it.salario }
+        .forEach { println(it) } //Exemplo de encadeamento
+
+    println("-----------------------------------------------")
+    println("Print de forma para agrupar por tipo de contratação: ")
+    funcionarios
+        .groupBy { it.tipoContratacao }
+        .forEach { println("$it\n") } //Exemplo de encadeamento
 
 }
 
 data class Funcionario(
     val nome: String,
-    val salario: Double
+    val salario: Double,
+    val tipoContratacao: String
 ){
     override fun toString(): String = """
-        Nome: $nome
-        Salário: R$ %.2f
-
+        Nome: $nome | Salário: R$ %.2f
     """.trimIndent().format(salario)
 }
