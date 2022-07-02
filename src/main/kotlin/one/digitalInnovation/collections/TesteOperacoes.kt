c5bec0bdcd425ad3fc7669103d1f287dc9642410
julioho7 <julioholiveira7@gmail.com>
2022-07-02 16:46:07 -0300

    Melhorando código de Operações

--- src/main/kotlin/one/digitalInnovation/collections/TesteOperacoes.kt
+++ src/main/kotlin/one/digitalInnovation/collections/TesteOperacoes.kt
@@ -1,18 +1,19 @@
 package one.digitalInnovation.collections
 
 fun main() {
-    val salarios = doubleArrayOf(1000.0, 2250.0, 4000.0)
+    val salarios = doubleArrayOf(1000.0, 2501.0, 4000.0)
 
     for(salario in salarios){
         println(salario)
     }
 
     println("--------------------------------------")
-    println("Média dos salários: %.2f".format(salarios.average()))
-    println("Maior salalário: %.2f".format(salarios.maxOrNull()))
-    println("Menor salalário: %.2f".format(salarios.minOrNull()))
+    println("Média dos salários: R$%.2f".format(salarios.average()))
+    println("Maior salalário: R$%.2f".format(salarios.maxOrNull()))
+    println("Menor salalário: R$%.2f".format(salarios.minOrNull()))
 
     println("--------------------------------------")
     val filtroSalariosMaiores = salarios.filter { it > 2500.0 }
-    filtroSalariosMaiores.forEach { println("%.2f".format(it)) }
+    println("Os salários maiores que R$2500,00 são: ")
+    filtroSalariosMaiores.forEach { println("R$%.2f".format(it)) }
 }
